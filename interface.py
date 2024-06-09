a3aaca55a762c6ff52a2febf018cfec54de26fa5
Tulio Leal <denkidev@gmail.com>
2024-06-09 13:27:15 -0300

    Backend e interface linkados

--- interface.py
+++ interface.py
@@ -1,12 +1,11 @@
 import customtkinter as ctk
-
+import Backend
 
 janela = ctk.CTk()
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 janela.title("Bot-LinkedIn")
 janela.iconbitmap("./assets/logo.ico")
-
 
 ctk.CTkLabel(janela, text="Email:").grid(row=0, column=0, padx=10, pady=5)
 email_entry = ctk.CTkEntry(janela)
@@ -29,11 +28,15 @@ mensagem_entry.grid(row=3, column=1, padx=10, pady=5)
 # Botão Iniciar
 def coletar_dados():
     email = email_entry.get()
-
-
+    senha = senha_entry.get()
+    publico_alvo = publico_alvo_entry.get()
+    mensagem = mensagem_entry.get("1.0", "end-1c")
+    print("Email: ", email, ", Senha: ", senha, ", publico alvo: ", publico_alvo, ", Mensagem: ", mensagem)
+    
+    Backend.iniciar_auto(email, senha, publico_alvo, mensagem)
+  
 iniciar_button = ctk.CTkButton(janela, text="Iniciar", command=coletar_dados,  fg_color="#4C856A")
 iniciar_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
-
 
 janela.mainloop()
    
