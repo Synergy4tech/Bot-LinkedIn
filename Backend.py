476be7e360f1baf7ef4ff2b4443cc66d9cfb3e7e
Rafael Damasceno <rafaeldamascenowhy@gmail.com>
2024-06-09 18:36:06 -0300

    :construction: feat: added the messaging system

--- Backend.py
+++ Backend.py
@@ -1,57 +1,68 @@
-#me lembrem de apagar os comentarios depois sksksksksksskskskskksksksksk
-"""
-termos a mudar:
-navegador = driver
-instancia = self
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+import time 
 
-"""
-
-from selenium import webdriver # Importa o webdriver do selenium todo mundo sabe, mas foda-se 
-from selenium.webdriver.common.keys import Keys # isso serve para conseguirmos dar o ENTER no campo da senha e logar
-import time # Importa o time para dar um tempo entre as ações do bot
-
-"""Aqui ele ja faz a chamada do chromedrive automaticamente, caso não tenha o chromedrive instalado, ele irá baixar automaticamente, ou seja não precisa instalar o chromedrive manualmente, o selenium faz isso automaticamente."""
-
-#aqui para a instancia eu coloquei instancia, mas poderia ser qualquer coisa ta, mas eu coloquei instancia para ficar mais facil de entender
-
-class Iniciar: #vou por classes para ficar mais facil para vcs entenderem 
-    def __init__(instancia, email, senha, publico_alvo, mensagem): 
-        instancia.navegador = webdriver.Chrome() #inicia o webdriver
-        instancia.email = email
-        instancia.senha = senha
-        instancia.publico_alvo = publico_alvo
-        instancia.mensagem = mensagem
-        #instancia.navegador.maximize_window() #maximiza a janela do webdriver
+class Iniciar: 
+    def __init__(self, email, senha, publico_alvo, mensagem): 
+        self.driver = webdriver.Chrome() 
+        self.email = email
+        self.senha = senha
+        self.publico_alvo = publico_alvo
+        self.mensagem = mensagem
         
-    def open_site(instancia): #função para abrir o site
+    def open_site(self): 
         time.sleep(2)
-        instancia.navegador.get("https://www.linkedin.com/login")
-        time.sleep(5)
+        self.driver.get("https://www.linkedin.com/login")
+        time.sleep(2)
 
-    def fazer_login_link(instancia):
-        email_entry = instancia.navegador.find_element('xpath', '//*[@id="username"]')
-        email_entry.send_keys(instancia.email)
-        senha_entry = instancia.navegador.find_element('xpath', '//*[@id="password"]')
-        senha_entry.send_keys(instancia.senha)
+    def fazer_login_link(self):
+        email_entry = self.driver.find_element('xpath', '//*[@id="username"]')
+        email_entry.send_keys(self.email)
+        senha_entry = self.driver.find_element('xpath', '//*[@id="password"]')
+        senha_entry.send_keys(self.senha)
         senha_entry.send_keys(Keys.RETURN)
-        time.sleep(5)
+        time.sleep(2)
     
-    def procura_alvos(instancia):
-        caixinha = instancia.navegador.find_element('xpath', "//input[contains(@class, 'search-global-typeahead__input')]")
-        caixinha.send_keys(instancia.publico_alvo)
+    def procura_alvos(self):
+        caixinha = self.driver.find_element('xpath', "//input[contains(@class, 'search-global-typeahead__input')]")
+        caixinha.send_keys(self.publico_alvo)
         caixinha.send_keys(Keys.RETURN)
-        time.sleep(5)
-
-    def mensagem_enviada(instancia):
-        print() #isso é so pra n acusar erro na função embaixo
+        time.sleep(2)
+        mais = self.driver.find_element('xpath', '/html/body/div[5]/div[3]/div[2]/div/div[1]/main/div/div/div[2]/div/div[2]/a')
+        mais.click()
+        time.sleep(2)
+        # Identificar a lista de conexões
+        connections_list_xpath = "/html/body/div[5]/div[3]/div[2]/div/div[1]/main/div/div/div[2]/div/ul"
+        # Pegar todos os botões de conectar na página
+        connect_buttons = self.driver.find_elements('xpath', f"{connections_list_xpath}//button[contains(@class, 'artdeco-button--2')]")
         
+        # Loop para clicar em cada botão de conectar e enviar uma mensagem
+        for index, button in enumerate(connect_buttons):
+            try:
+                # Clicar no botão "Conectar"
+                button.click()
+                time.sleep(2)
+                # Encontrar a caixa de mensagem e inserir a mensagem personalizada
+                add_note_button = self.driver.find_element('xpath', '//button[@aria-label="Adicionar nota"]')
+                add_note_button.click()
+                time.sleep(1)
 
+                message_box = self.driver.find_element('xpath', '//*[@id="custom-message"]')
+                message_box.send_keys(self.mensagem)
+                message_box.send_keys(Keys.RETURN)
 
+                time.sleep(5)
+
+                send_button = self.driver.find_element('xpath', '//button[@aria-label="Enviar convite"]')
+                send_button.click()
+                time.sleep(5)
+
+            except Exception as e:
+                print(f"Ocorreu um erro ao processar o botão {index + 1}: {e}")
+
+        
 def iniciar_auto(email, senha, publico_alvo, mensagem):
     bot = Iniciar(email, senha, publico_alvo, mensagem)
     bot.open_site()
     bot.fazer_login_link()
     bot.procura_alvos()
-    bot.mensagem_enviada()
-
-
